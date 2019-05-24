d03b4888161ad00f3068c62bdb5ffe9b896c958a
jarosciak@gmail.com <Gith1974>
2019-05-24 10:55:31 -0400

    db and script changes

--- slovak-diacritics.py
+++ slovak-diacritics.py
@@ -2,35 +2,66 @@ import keyboard
 import re
 import sqlite3
 import unidecode
+import pyperclip
 
 words = ''
+found_words = []
+last_word = ''
+offset = 1
+right_press_counter = 0
+
+
+def db_search_to_list(search_term):
+    global found_words
+    conn = sqlite3.connect('sk.db')
+    cursor = conn.execute("select word from sk_aspell where lower(worddf) = '" + search_term.lower() + "'")
+    for row in cursor:
+        if search_term[0].isupper():
+            found_words.append(str(row[0]).capitalize())
+        else:
+            found_words.append(str(row[0]))
+    #print(found_words)
+
+
+def place_word(found_word):
+    for x in range(len(found_word)+1):
+        keyboard.press('backspace')
+    pyperclip.copy(found_word)
+    keyboard.send('ctrl+v')
+    keyboard.press(' ')
 
 
 def on_press_reaction(event):
-    global words
-    conn = sqlite3.connect('sk.db')
+    global words, found_words, last_word, right_press_counter
     if event.name == 'space' or event.name == '.' or event.name == ',' or event.name == ';' or event.name == 'enter':
-        if len(str(words))>0:
+        if len(str(words)) > 0:
             search = unidecode.unidecode(words)
-            cursor = conn.execute("select distinct(word) from sk_aspell where length(word)>2 AND lower(worddf)<>lower(word) AND lower(worddf) = '"+search.lower()+"' limit 1")
-            for row in cursor:
-                found_word = row[0]
-                if search[0].isupper():
-                    found_word = str(found_word).capitalize()
-            print(words + ": " + found_word)
+            found_words = [search]
+            right_press_counter = 1
+            db_search_to_list(search)
         words = ''
     elif event.name == 'backspace':
         if len(words) > 0:
             words = words[:-1]
+    elif event.name == 'delete':
+        found_words = []
+    elif event.name == 'right':
+        if len(found_words)>1:
+            #print(str(right_press_counter) + " - " + found_words[right_press_counter])
+            place_word(str(found_words[right_press_counter]))
+            right_press_counter = right_press_counter + 1
+            #print("Counter: " + str(right_press_counter))
+            if right_press_counter > len(found_words)-1:
+                right_press_counter = 0
     else:
+        #print(event.name)
         if len(str(event.name)) == 1:
             if re.match("^[A-Za-z0-9_-]*$", event.name):
                 words = words + str(event.name)
-    conn.close()
+    #conn.close()
 
 
 keyboard.on_press(on_press_reaction)
 
 while True:
     pass
-
