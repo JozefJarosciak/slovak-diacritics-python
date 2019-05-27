1b562673fbeb0c8aceb908f488408decc69de7d7
jarosciak@gmail.com <Gith1974>
2019-05-27 05:42:46 -0400

    script changes

--- slovak-diacritics.py
+++ slovak-diacritics.py
@@ -1,14 +1,11 @@
 import re
 import sqlite3
 import keyboard
-import pyperclip
 import unidecode
 
 words = ''
 found_words = []
-last_word = ''
-offset = 1
-right_press_counter = 0
+equal_sign_press_counter = 0
 
 
 def db_search_to_list(search_term):
@@ -20,45 +17,38 @@ def db_search_to_list(search_term):
             found_words.append(str(row[0]).capitalize())
         else:
             found_words.append(str(row[0]))
-    print(found_words)
+    if len(found_words)>1:
+        print(found_words)
 
 
-def place_word(found_word):
+def place_word(found_word, trigger):
     for x in range(len(found_word)+1):
         keyboard.send('backspace')
-    pyperclip.copy(found_word)
-    keyboard.send('ctrl+v')
-    keyboard.press(' ')
-    pyperclip.copy('')
+    keyboard.write(found_word)
 
 
 def on_press_reaction(event):
-    global words, found_words, last_word, right_press_counter
+    global words, found_words, equal_sign_press_counter
     if event.name == 'space' or event.name == '.' or event.name == ',' or event.name == ';' or event.name == 'enter':
-        if len(str(words)) > 0:
-            search = unidecode.unidecode(words)
-            found_words = [search]
-            right_press_counter = 1
-            db_search_to_list(search)
         words = ''
     elif event.name == 'backspace':
         if len(words) > 0:
             words = words[:-1]
     elif event.name == 'delete':
         found_words = []
-    elif event.name == 'right':
+    elif event.name == '=':
         if len(str(words)) > 0:
             search = unidecode.unidecode(words)
             found_words = [search]
-            right_press_counter = 1
+            equal_sign_press_counter = 1
             db_search_to_list(search)
         words = ''
         if len(found_words) > 1:
-            print(str(right_press_counter) + " - " + found_words[right_press_counter])
-            place_word(str(found_words[right_press_counter]))
-            right_press_counter = right_press_counter + 1
-            if right_press_counter > len(found_words)-1:
-                right_press_counter = 0
+            print(str(equal_sign_press_counter) + " - " + found_words[equal_sign_press_counter])
+            place_word(str(found_words[equal_sign_press_counter]), str(event.name))
+            equal_sign_press_counter = equal_sign_press_counter + 1
+            if equal_sign_press_counter > len(found_words)-1:
+                equal_sign_press_counter = 0
     else:
         # print(event.name)
         if len(str(event.name)) == 1:
