06a59131c1dbfc2d6d86cbc05e0d43f8856c24ab
jjaros01 <Joe@doprocess.com>
2019-05-29 14:12:32 -0400

    code improvements

--- README.md
+++ README.md
@@ -3,3 +3,8 @@ Automaticke pridavanie Slovenskej Diakritiky v texte
 
 More at:
 https://www.joe0.com/2019/05/25/automatically-adding-foreign-diacritics-accents-anywhere-in-windows-linux-using-python-with-global-keyboard-hooks-embedded-sqlite-db/
+
+
+To Deploy Windows Executable:
+
+pyinstaller -y -i "C:/code/slovak-diacritics/sk.ico" --add-data "C:/code/slovak-diacritics/sk.ico";"." --add-data "C:/code/slovak-diacritics/sk.db";"." "C:/code/slovak-diacritics/slovak-diacritics.py"
--- slovak-diacritics.py
+++ slovak-diacritics.py
@@ -3,10 +3,94 @@ import sqlite3
 import time
 import keyboard
 import unidecode
+import tkinter
+from tkinter import messagebox
+from sys import exit
 
 words = ''
 found_words = []
 equal_sign_press_counter = 0
+showWindow = True
+
+# Initialize the main window
+window = tkinter.Tk()
+window.minsize(400, 200)
+window.title("Slovenská Diakritika vs 1.0")
+window.wm_iconbitmap("sk.ico")
+
+
+def character_limit(entry_text):
+    if len(entry_text.get()) > 0:
+        entry_text.set(entry_text.get()[-1])
+
+
+# Place UI components
+
+labelUpdateTrigger = tkinter.Label(window, text="Zmeniť Spúšťač:")
+labelUpdateTrigger.place(x=10, y=10)
+
+trigger = tkinter.Entry(window, width=3)
+trigger.place(x=100, y=10)
+trigger.insert(0, "=")
+
+labelAddWord = tkinter.Label(window, text="Pridať Slovo:")
+labelAddWord.place(x=10, y=30)
+
+addWord = tkinter.Entry(window, width=30)
+addWord.place(x=100, y=30)
+
+label = tkinter.Label(window, text="Posledné nájdene slovo:")
+label.place(x=10, y=180)
+
+
+# Define button press function
+def press():
+    # set label
+    try:
+        my_list = [str(addWord.get()).lower(), unidecode.unidecode(str(addWord.get()).lower())]
+        DBconnection = sqlite3.connect('sk.db')
+        c = DBconnection.cursor()
+        c.execute('INSERT INTO "sk_aspell" ("word", "worddf") VALUES (?,?)', my_list)
+        DBconnection.commit()
+        messagebox.showinfo(title="Databáza",
+                            message=f"Slovo '{str(addWord.get()).lower()}' bolo úspešne vložené do databázy")
+        print(my_list)
+    except:
+        messagebox.showinfo(title="Databáza",
+                            message=f"Slovo '{str(addWord.get()).lower()}' sa už nachádza v databáze!")
+
+
+# Place 'Change Label' button on the window
+button = tkinter.Button(window, text="Vlož", command=press)
+# Place label at coordinates x=10 and y=10 (top right hand corner)
+button.place(x=290, y=28)
+
+
+def on_closing():
+    if messagebox.askokcancel("Ukončiť Program", "Chcete ukončiť program?"):
+        window.destroy()
+        exit()
+
+
+window.protocol("WM_DELETE_WINDOW", on_closing)
+
+
+# Function to find the screen dimensions, calculate the center and set geometry
+def center(win):
+    # Call all pending idle tasks - carry out geometry management and redraw widgets.
+    win.update_idletasks()
+    # Get width and height of the screen
+    width = win.winfo_width()
+    height = win.winfo_height()
+    # Calculate geometry
+    x = (win.winfo_screenwidth() // 2) - (width // 2)
+    y = (win.winfo_screenheight() // 2) - (height // 2)
+    # Set geometry
+    win.geometry('{}x{}+{}+{}'.format(width, height, x, y))
+
+
+# Center Window on Screen
+center(window)
 
 
 def db_search_to_list(search_term):
@@ -37,7 +121,7 @@ def on_press_reaction(event):
             words = words[:-1]
     elif event.name == 'delete':
         found_words = []
-    elif event.name == '=':
+    elif event.name == str(trigger.get()):
         if len(str(words)) > 0:
             search = unidecode.unidecode(words)
             found_words = [search]
@@ -46,6 +130,7 @@ def on_press_reaction(event):
         words = ''
         if len(found_words) > 1:
             print(str(equal_sign_press_counter) + " - " + found_words[equal_sign_press_counter])
+            label.config(text=f"Posledné nájdene slovo: {found_words[equal_sign_press_counter]}")
             place_word(str(found_words[equal_sign_press_counter]))
             equal_sign_press_counter = equal_sign_press_counter + 1
             if equal_sign_press_counter > len(found_words) - 1:
@@ -60,5 +145,8 @@ def on_press_reaction(event):
 keyboard.on_press(on_press_reaction)
 
 while True:
-    time.sleep(0.05)
+    if showWindow:
+        window.mainloop()
+        showWindow = False
+    time.sleep(0.01)
     pass
